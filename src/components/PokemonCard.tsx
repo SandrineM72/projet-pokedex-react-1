7c8f002470174c548fcbc6cc57c34b9551ce30b8
SandrineM72 <sandrine.maingourd.pro72@gmail.com>
2025-11-05 16:13:09 +0100

    Modification du component PokemonCard.tsx

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,13 +1,45 @@
+const pokemonList = [
+
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+
+  {
+    name: "mew",
+  },
+
+    {
+    name: "Koraidon",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1007.png",
+  },
+
+];
+
+
 function PokemonCard() {
+    const pokemon = pokemonList[1];
+
   return (
 
     <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokemon Bulbasaur" />
+        {pokemon.imgSrc !== undefined ? (
+            <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+            <p><i>image manquante</i></p>
+        )}
+    
         <figcaption>
-            <p>Bulbasaur</p>
+            <p>{pokemon.name}</p>
         </figcaption>
     </figure>
   );
 }
 
 export default PokemonCard;
+
+
+
+// Site pokemons : https://github.com/PokeAPI/sprites/tree/master/sprites/pokemon/other/official-artwork
